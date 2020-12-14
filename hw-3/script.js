ce8b3c6ec70ad850879ee9fab687ba259bc8da76
Uros Stanojkov <66559472+uroskg99@users.noreply.github.com>
2020-12-14 17:00:25 +0100

    Commit 9 - Big changes in JavaScript file
    Added functions that will load questions and answers. Added function for Next Question button.

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,5 +1,6 @@
-var btn1 = document.getElementById('submit');
-var btn2 = document.getElementById('dugme-pravila');
+const btn1 = document.getElementById('submit');
+const btn2 = document.getElementById('dugme-pravila');
+
 
 if (btn1){
     btn1.addEventListener('click', promeniStranu1);
@@ -28,61 +29,149 @@ function promeniStranu2(){
 const pitanjaJSON = `[
     {
         "pitanje": "Koje godine je poceo prvi svetski rat?",
-        "odgovor1": "1915",
-        "odgovor2": "1914",
-        "odgovor3": "1918",
-        "odgovor4": "1922"
+        "odgovori": [
+            { "tekst": "1915", "tacno": "false"},
+            { "tekst": "1914", "tacno": "true"},
+            { "tekst": "1918", "tacno": "false"},
+            { "tekst": "1925", "tacno": "false"}
+        ]
     },
     {
         "pitanje": "Izbaci uljeza:",
-        "odgovor1": "HTML",
-        "odgovor2": "JavaScript",
-        "odgovor3": "CSS",
-        "odgovor4": "PHP"
+        "odgovori": [
+            { "tekst": "JavaScript", "tacno": "false"},
+            { "tekst": "CSS", "tacno": "false"},
+            { "tekst": "HTML", "tacno": "false"},
+            { "tekst": "PHP", "tacno": "true"}
+        ]
     },
     {
         "pitanje": "Katalonija je deo:",
-        "odgovor1": "Portugala",
-        "odgovor2": "Maroka",
-        "odgovor3": "Spanije",
-        "odgovor4": "Tunisa"
+        "odgovori": [
+            { "tekst": "Portugala", "tacno": "false"},
+            { "tekst": "Maroka", "tacno": "false"},
+            { "tekst": "Spanije", "tacno": "true"},
+            { "tekst": "Tunisa", "tacno": "false"}
+        ]
     },
     {
         "pitanje": "Had je bog cega?",
-        "odgovor1": "Rata",
-        "odgovor2": "Vode",
-        "odgovor3": "Podzemnog sveta",
-        "odgovor4": "Ljubavi"
+        "odgovori": [
+            { "tekst": "Rata", "tacno": "false"},
+            { "tekst": "Vode", "tacno": "false"},
+            { "tekst": "Ljubavi", "tacno": "false"},
+            { "tekst": "Podzemnog sveta", "tacno": "true"}
+        ]
     },
     {
         "pitanje": "Ko je otkrio penicilin?",
-        "odgovor1": "Johan Mendel",
-        "odgovor2": "Marija Kiri",
-        "odgovor3": "Luj Paster",
-        "odgovor4": "Aleksandar Fleming"
+        "odgovori": [
+            { "tekst": "Johan Mendel", "tacno": "false"},
+            { "tekst": "Marija Kiri", "tacno": "false"},
+            { "tekst": "Luj Paster", "tacno": "false"},
+            { "tekst": "Aleksandar Fleming", "tacno": "true"}
+        ]
     },
     {
         "pitanje": "Koliko ima kostiju u telu odraslog coveka?",
-        "odgovor1": "206",
-        "odgovor2": "270",
-        "odgovor3": "301",
-        "odgovor4": "223"
+        "odgovori": [
+            { "tekst": "206", "tacno": "true"},
+            { "tekst": "227", "tacno": "false"},
+            { "tekst": "223", "tacno": "false"},
+            { "tekst": "210", "tacno": "false"}
+        ]
     },
     {
         "pitanje": "Koji je najveci grad Amerike?",
-        "odgovor1": "Vasington",
-        "odgovor2": "Njujork",
-        "odgovor3": "Boston",
-        "odgovor4": "Filadelfija"
+        "odgovori": [
+            { "tekst": "Filadelfija", "tacno": "false"},
+            { "tekst": "Njujork", "tacno": "true"},
+            { "tekst": "Boston", "tacno": "false"},
+            { "tekst": "Vasington", "tacno": "false"}
+        ]
     },
     {
         "pitanje": "Kako se zove glavni glumac u filmu John Wick?",
-        "odgovor1": "Keanu Reeves",
-        "odgovor2": "Liam Neeson",
-        "odgovor3": "Christian Bale",
-        "odgovor4": "Tom Hardy"
+        "odgovori": [
+            { "tekst": "Keanu Reeves", "tacno": "true"},
+            { "tekst": "Liam Neeson", "tacno": "false"},
+            { "tekst": "Christian Bale", "tacno": "false"},
+            { "tekst": "Tom Hardy", "tacno": "false"}
+        ]
     }
 ]`
 
-pitanja = JSON.parse(pitanjaJSON);
-console.log(pitanja[0].odgovor1);
+var pitanjaJS = JSON.parse(pitanjaJSON);
+var mesajPitanja, trenutniIndeksPitanja
+var pitanje = document.getElementById('pitanje');
+var ponudjeniOdgovori = document.getElementById('ponudjeni-odgovori');
+
+var sledecePitanjeDugme = document.getElementById('nastavi');
+var odustaniDugme = document.getElementById('odustani');
+var zapocniIgru = document.getElementById('zapocniIgru');
+zapocniIgru.addEventListener('click', pocniIgru);
+sledecePitanjeDugme.addEventListener('click', () => {
+    trenutniIndeksPitanja++;
+    postaviPitanje();
+})
+
+
+function pocniIgru(){
+    zapocniIgru.classList.add('sakrij');
+    ponudjeniOdgovori.classList.remove('sakrij');
+    pitanje.classList.remove('sakrij');
+    sledecePitanjeDugme.classList.remove('sakrij');
+    odustaniDugme.classList.remove('sakrij');
+
+    mesajPitanja = pitanjaJS.sort(() => Math.random() - .5);
+    trenutniIndeksPitanja = 0;
+    postaviPitanje();
+}
+
+function postaviPitanje(){
+    restartuj();
+    prikaziPitanje(mesajPitanja[trenutniIndeksPitanja]);
+}
+
+function restartuj(){
+    while (ponudjeniOdgovori.firstChild) {
+        ponudjeniOdgovori.removeChild(ponudjeniOdgovori.firstChild);
+    }
+}
+
+function prikaziPitanje(postaviPitanje){
+    pitanje.innerText = postaviPitanje.pitanje;
+    postaviPitanje.odgovori.forEach(odgovor => {
+        const dugme = document.createElement('button');
+        dugme.innerText = odgovor.tekst;
+        dugme.classList.add('odgovori');
+        if (odgovor.tacno) {
+            dugme.dataset.tacno = odgovor.tacno;
+        }
+        dugme.addEventListener('click', izaberiOdgovor);
+        ponudjeniOdgovori.appendChild(dugme);
+    });
+}
+
+function izaberiOdgovor(e){
+    const izabranOdgovor = e.target;
+    /*const tacno = izabranOdgovor.dataset.tacno;
+    Array.from(ponudjeniOdgovori.children).forEach(dugme => {
+        statusOdgovora(dugme, dugme.dataset.tacno);
+    });*/
+    
+}
+
+/*function statusOdgovora(element, tacno){
+    ocistiStatus(element);
+    if (tacno){
+        element.classList.add('tacno');
+    } else {
+        element.classList.add('netacno');
+    }
+}
+
+function ocistiStatus(element){
+    element.classList.remove('tacno');
+    element.classList.remove('netacno');
+}*/
