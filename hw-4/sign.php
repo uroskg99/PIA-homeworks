f1465cfdd01150038c099008a8ead10802ac4e2b
Uros Stanojkov <66559472+uroskg99@users.noreply.github.com>
2021-01-16 16:51:24 +0100

    Commit 4 - Deleted comments

--- hw-4/sign.php
+++ hw-4/sign.php
@@ -8,7 +8,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="registersign.css">
 </head>
 
 <body>
@@ -53,12 +53,7 @@
         $query = "SELECT * FROM `users` WHERE ( username='$login' OR email = '$login') and password='$password'";
         $result = mysqli_query($conn, $query);
         $row = $result->fetch_assoc();
-        /*if($result){
-            while($row=mysqli_fetch_array($result)){
-                header('Location: home.php');
-            }
-            $msgWrong = "Sorry, your password or email are incorrect";
-        }*/
+        
 
         session_regenerate_id();
         $_SESSION['username'] = $row['username'];
@@ -72,65 +67,9 @@
             header("location:homeadmin.php");
         }
 
+
     }
-
-
-    /*if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-        $myuser = mysql_real_escape_string($db, $_POST['user']);
-        $mypassword = mysql_real_escape_string($db, $_POST['password']);
-
-        $sql = "SELECT id FROM admin WHERE username = '$myuser' and password = '$mypassword'";
-        $result = mysqli_query($db, $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $active = $row['active'];
-
-        $count = mysqli_num_rows($result);
-
-        if($count == 1){
-            session_register("myuser");
-            $_SESSION['login_user'] = $myuser;
-
-            header("location: welcome.php");
-        }else{
-            $error = "invalid username or password";
-        }
-    }
-
-    $username = $_POST['user'];
-    $password = $_POST['password'];
-
-    $username = stripcslashes($username);
-    $password = stripcslashes($password);
-
-    $username = mysql_real_escape_string($username);
-    $password = mysql_real_escape_string($password);
-
-    mysql_connect("localhost", "root", "");
-    mysql_select_db("users");
-
-    $result = mysql_query("select * from users where username = '$username' and password = '$password'")
-        or die("Failed to query database ".mysql_error());
-
-    $row = mysql_fetch_array($result);
-
-    if($row['username'] == $username && $row['password'] == $password){
-        echo "login successfully, welcome".$row['username'];
-    }else{
-        echo "failed to login";
-    }
-
-    $username = $_POST['user'];
-    $password = $_POST['password'];
-
-    $s = " select * from users where username = '$username'";
-    $result = mysqli_query($con, $s);
-
-    $num = mysqli_num_rows($result);
-
-    if($num == 1){
-        echo "";
-    }*/
+    
 
     ?>
 
