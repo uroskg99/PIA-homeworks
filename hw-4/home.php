50165d6580fad56778a89ea66c45aff7f3ce7ae4
Uros Stanojkov <66559472+uroskg99@users.noreply.github.com>
2021-01-16 16:45:05 +0100

    Commit 2 - Added styling for 'home.php'

--- hw-4/home.php
+++ hw-4/home.php
@@ -1,5 +1,32 @@
 <?php
-
-echo "USER";
-
+include 'config.php';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IMDB Copy</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="home.css">
+
+</head>
+<body>
+    <div class="container-fluid header">
+        <a href="home.php">
+            <img src="img/imdb-logo.png" class="logo">
+        </a>
+        
+        <span class="login-info"><?php echo "Logged in as user"; ?></span>
+        <a href="">
+            <button class="customBtn" id="signUp">Logout</button>
+        </a>
+    </div>
+  
+
+</body>
+</html>
