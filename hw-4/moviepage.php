b39e96aa2d7f82585e750393e4250c752c25a6da
Uros Stanojkov <66559472+uroskg99@users.noreply.github.com>
2021-01-19 02:56:58 +0100

    Commit 10 - Small changes in 'register.php' and 'sign.php'
    Pictures will now load on the Html file from the base.

--- hw-4/moviepage.php
+++ hw-4/moviepage.php
@@ -12,8 +12,9 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="home.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="moviepage.css">
 
 </head>
 <body>
@@ -39,44 +40,47 @@ session_start();
     while($row = mysqli_fetch_assoc($result)){
 
     ?>
-    
-    <div class="container tableMovie">
-        <h2><?php echo $row['title']; ?></h2>
-        <p><?php echo $row['description']; ?></p>
-        <table class="table">
-            <tbody>
-            <tr class="table-secondary">
-                <td>Genre</td>
-                <td><?php echo $row['genre']; ?></td>
-            </tr>    
-            <tr class="table-secondary">
-                <td>Scenarist</td>
-                <td><?php echo $row['scenarist']; ?></td>
-            </tr> 
-            <tr class="table-secondary">
-                <td>Director</td>
-                <td><?php echo $row['director']; ?></td>
-            </tr> 
-            <tr class="table-secondary">
-                <td>Production</td>
-                <td><?php echo $row['production']; ?></td>
-            </tr> 
-            <tr class="table-secondary">
-                <td>Actors</td>
-                <td><?php echo $row['actors']; ?></td>
-            </tr> 
-            <tr class="table-secondary">
-                <td>Year</td>
-                <td><?php echo $row['year']; ?></td>
-            </tr> 
-            <tr class="table-secondary">
-                <td>Duration</td>
-                <td><?php echo $row['duration']; ?></td>
-            </tr> 
-               
-        </tbody>
-    </table>
-</div>
+    <div class="all-info">
+        <div class="container table-movie">
+            <h2><?php echo $row['title']; ?></h2>
+            <p class="desc"><?php echo $row['description']; ?></p>
+            <table class="table table-hover">
+                <tbody>
+                <tr class="table-secondary">
+                    <td class="information">Genre</td>
+                    <td><?php echo $row['genre']; ?></td>
+                </tr>    
+                <tr class="table-secondary">
+                    <td class="information">Scenarist</td>
+                    <td><?php echo $row['scenarist']; ?></td>
+                </tr> 
+                <tr class="table-secondary">
+                    <td class="information">Director</td>
+                    <td><?php echo $row['director']; ?></td>
+                </tr> 
+                <tr class="table-secondary">
+                    <td class="information">Production</td>
+                    <td><?php echo $row['production']; ?></td>
+                </tr> 
+                <tr class="table-secondary">
+                    <td class="information">Actors</td>
+                    <td><?php echo $row['actors']; ?></td>
+                </tr> 
+                <tr class="table-secondary">
+                    <td class="information">Year</td>
+                    <td><?php echo $row['year']; ?></td>
+                </tr> 
+                <tr class="table-secondary">
+                    <td class="information">Duration</td>
+                    <td><?php echo $row['duration']; ?></td>
+                </tr> 
+                
+                </tbody>
+            </table>
+        </div>
+
+        <img class="movie-pic" src=<?php echo $row['picture']; ?> >
+    </div>
 
 <?php
 }
