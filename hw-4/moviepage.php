07476fc6ac1e96e38978e7bc2127e4e54db2f4d4
Uros Stanojkov <66559472+uroskg99@users.noreply.github.com>
2021-01-30 20:52:52 +0100

    Commit 29 - Added option for user to rate movie
    User can rate any movie he wants. If he has already rated chosen movie, he is able to update his rating for that movie. He will see his previous rate and message that he has already rated the movie.

--- hw-4/moviepage.php
+++ hw-4/moviepage.php
@@ -24,6 +24,7 @@ if (!isset($_SESSION['username'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="moviepage2.css">
+    
 
 </head>
 <body>
@@ -39,13 +40,15 @@ if (!isset($_SESSION['username'])) {
 
     <?php 
 
-    $title = mysqli_real_escape_string($conn, $_GET['title']);
+    
+    $title_srch = mysqli_real_escape_string($conn, $_GET['title']);
 
-    $sql = "SELECT * FROM movies WHERE title='$title'";
+    $sql = "SELECT * FROM movies WHERE title='$title_srch'";
     $result = mysqli_query($conn, $sql);
     $query_res = mysqli_num_rows($result);
 
     while($row = mysqli_fetch_assoc($result)){
+        $title_new = $row['title']; 
 
     ?>
 
@@ -73,14 +76,83 @@ if (!isset($_SESSION['username'])) {
 
         </div>
 
+        <?php
+            $qry_rate = "SELECT * FROM rating WHERE title = '$title_new' ";
+            $res_rate = mysqli_query($conn, $qry_rate);
+
+            $sum = 0;
+            $n = 0;
+            $currentUser = $_SESSION['username'];
+            $msg = '';
+            $msg2 = '';
+            $currentRate = 'Enter';
+
+            if (mysqli_num_rows($res_rate) > 0){
+                while($row_rate = mysqli_fetch_assoc($res_rate)){
+                    $sum += $row_rate['rate'];
+                    $n++;
+
+
+                    if($row_rate['user'] == $currentUser && $row_rate['title'] == $row['title']){
+                        $msg = "You have already rated this movie, you can edit!";
+                        $currentRate = $row_rate['rate'];
+                    
+                    }
+                }
+
+                if($n>0){
+                    $sumFinal = $sum / $n;
+                    $msg2 = $sumFinal."/10";
+                }else{
+                    $msg2 = "No results";
+                }
+
+                if(isset($_POST['rate'])){
+                    $rating = $_POST['rating'];
+
+                    $query_check = "SELECT title, user FROM rating";
+                    $unique = 0;
+                    $result_check = mysqli_query($conn, $query_check);
+                    
+                    while($row_check = mysqli_fetch_array($result_check)){
+                        if($title_new == $row_check['title'] && $currentUser == $row_check['user']){
+                            $unique = 1;
+                            break;
+                        }        
+                    }
+
+                    if($unique == 0){
+                        $query_new = "INSERT INTO rating (title,user,rate) VALUES ('$title_new', '$currentUser', '$rating')";
+                        $res = mysqli_query($conn, $query_new);
+                        header("location:home.php");
+
+                    }else{
+                        $query_new = " UPDATE rating SET rate = '$rating' WHERE title = '$title_new' and user = '$currentUser' ";
+                        $res = mysqli_query($conn, $query_new);
+                        header("location:home.php");
+                    }
+
+                
+                }
+
+
+        ?>
+
+
+
         <hr>
         <div class="rating">
             <div class="row">
                 <div class="col-sm-6">
-                    Ocene
+                    <form action="" method="POST">
+                        <button name="rate" onclick='return confirmrate()'>Rate</button>
+                        <span><input type="number" name="rating" value=<?php echo $currentRate; ?> placeholder="Enter Your rate!"></span>
+                        <br>
+                        <?php echo $msg; ?>
+                    </form>
                 </div>
                 <div class="col-sm-6">
-                    Ukupne ocene
+                    Rating : <?php echo $msg2; ?> | Number of rates : <?php echo $n; ?>
                 </div>
             </div>
         </div>
@@ -89,11 +161,15 @@ if (!isset($_SESSION['username'])) {
 
     
 
-<?php
-}
-?>
+    <?php
+    }
+    ?>
     
-
+<script>
+    function confirmrate(){
+        return confirm('Please, confirm if You are sure You want to rate this movie');
+    }
+</script>
 
 </body>
 </html>
